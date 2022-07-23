369c03cbf54e0ef0b80f163534c861315f861bc8
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-07-23 17:30:28 -0500

    firebase

--- src/base.js
+++ src/base.js
@@ -2,10 +2,10 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs",
-  authDomain: "catch-of-the-day-wes-bos-2.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-wes-bos-2.firebaseio.com"
-});
+    apiKey: "AIzaSyB0fPFifLH4rnxWrgIhnZcL_pobqKo9O7E",
+    authDomain: "catch-of-the-day-2-adam-smith.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-2-adam-smith-default-rtdb.firebaseio.com"
+  });
 
 const base = Rebase.createClass(firebaseApp.database());
 
